b16bf80a3e9d5b196050a0e9bace6d6d83f6370a
Envy <sungin0406@naver.com>
2023-11-21 15:34:44 +0900

    Feat #1 11/20일자 커밋

---
+++ chap01/src/sec03/exam02/VariableInitializationExample.java
@@ -0,0 +1,15 @@
+package sec03.exam02;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 10;
+		
+		// value에 값이 없으므로 오류 
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}
