86ccb3592d70c3089f135b2a4fda1f653991bf09
Envy <sungin0406@naver.com>
2023-11-22 17:11:10 +0900

    Feat #2 sec02 examples

---
+++ chap03/src/sec02/exam05/CharOperationExample.java
@@ -0,0 +1,14 @@
+package sec02.exam05;
+
+public class CharOperationExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A' + 1; //char c1 = 66;
+		char c2 = 'A';
+		//char c3 = c2 + 1; //char변수가 산술 연산에 사용되면 int 타입으로 변환되어 연산결과가 int타입이 된다.
+		System.out.println("c1 : " + c1);
+		System.out.println("c2 : " + c2);
+		//System.out.println("c3 : " + c3);
+	}
+
+}
---
+++ chap03/src/sec02/exam09/LogicalOperatorExample.java
@@ -0,0 +1,26 @@
+package sec02.exam09;
+
+public class LogicalOperatorExample {
+
+	public static void main(String[] args) {
+		int charCode = 'A';
+		
+		if( (charCode>=65) & (charCode<=90))
+			System.out.println("대문자네");
+		
+		if((charCode>=97) && (charCode<=122))
+			System.out.println("소문자네");
+		
+		if((charCode<48) && !(charCode>57))
+			System.out.println("0~9사이 숫자군요");
+		
+		int value = 6;
+		
+		if((value%2==0) | (value%3==0))
+			System.out.println("2 또는 3의 배수");
+		
+		if((value%2==0) || (value%3==0))
+			System.out.println("2 또는 3의 배수");
+	}
+
+}
