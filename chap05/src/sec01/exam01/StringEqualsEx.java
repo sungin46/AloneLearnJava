04e596d0cf6879c666e11f23261cdb5c21f62ff0
Envy <sungin0406@naver.com>
2023-11-24 16:27:34 +0900

    Feat #4 sec01

---
+++ chap05/src/sec01/exam01/StringEqualsEx.java
@@ -0,0 +1,34 @@
+package sec01.exam01;
+
+public class StringEqualsEx {
+
+	public static void main(String[] args) {
+		String strVar1 = "홍성인";
+		String strVar2 = "홍성인";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("strVar1과 strVar2는 참조가 같다.");
+		}else {
+			System.out.println("strVar1과 strVar2는 참조가 다르다.");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1과 strVar2는 문자열이 같다");
+		}
+		
+		String strVar3 = new String("Sungin");
+		String strVar4 = new String("Sungin");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("strVar3과 strVar4는 참조가 같다.");
+		}else {
+			System.out.println("strVar3과 strVar4는 참조가 다르다.");
+		}
+		
+		if(strVar3.equals(strVar4)) {
+			System.out.println("strVar3과 strVar4는 문자열이 같다.");
+		}
+
+	}
+
+}
