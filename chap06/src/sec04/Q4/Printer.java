4ac29114c085f55bccb4b9088560305d3b065f98
Envy <sungin0406@naver.com>
2023-11-27 19:59:32 +0900

    Feat #5 sec04 확인문제 풀이

---
+++ chap06/src/sec04/Q4/Printer.java
@@ -0,0 +1,20 @@
+package sec04.Q4;
+
+public class Printer {
+	
+	void println(int print) {
+		System.out.println(print);
+	}
+	
+	void println(boolean print) {
+		System.out.println(print);
+	}
+	
+	void println(double print) {
+		System.out.println(print);
+	}
+	
+	void println(String print) {
+		System.out.println(print);
+	}
+}
