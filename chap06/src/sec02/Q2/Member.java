c83dfc9150359457d03caff3801bb5fdc2b81190
Envy <sungin0406@naver.com>
2023-11-27 17:04:46 +0900

    Feat #5 sec02 확인문제 풀

---
+++ chap06/src/sec02/Q2/Member.java
@@ -0,0 +1,8 @@
+package sec02.Q2;
+
+public class Member {
+	String name;
+	String id;
+	String password;
+	int age;
+}
---
+++ chap06/src/sec02/Q2/MemberEx.java
@@ -0,0 +1,11 @@
+package sec02.Q2;
+
+public class MemberEx {
+
+	public static void main(String[] args) {
+		Member member = new Member();
+		member.name = "홍성인";
+		member.age = 23;
+	}
+
+}
