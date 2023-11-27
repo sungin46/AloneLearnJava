742e641a894587f8764cbb042c38c29cff2f39af
Envy <sungin0406@naver.com>
2023-11-27 18:06:07 +0900

    Feat #5 sec03 확인문제 풀이

---
+++ chap06/src/sec03/Q2/Member.java
@@ -0,0 +1,11 @@
+package sec03.Q2;
+
+public class Member {
+	String name;
+	String id;
+	
+	Member(String name, String id){
+		this.name = name;
+		this.id = id;
+	}
+}
---
+++ chap06/src/sec03/Q4/BoardQ4.java
@@ -0,0 +1,12 @@
+package sec03.Q4;
+
+public class BoardQ4 {
+
+	public static void main(String[] args) {
+		Board board1 = new Board("타이틀", "빈 콘텐츠");
+		Board board2 = new Board("타이틀", "빈 콘텐츠", "홍성인");
+		Board board3 = new Board("타이틀", "빈 콘텐츠", "홍성인", "2023-11-27");
+		Board board4 = new Board("타이틀", "빈 콘텐츠", "홍성인", "2023-11-27", 100000);
+	}
+
+}
