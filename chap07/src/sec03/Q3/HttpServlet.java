30cd47f34dfcf1bf359037e72acedd1779e04e3f
Envy <sungin0406@naver.com>
2023-12-06 16:26:50 +0900

    Feat #6 sec03 확인문제 풀이

---
+++ chap07/src/sec03/Q3/HttpServlet.java
@@ -0,0 +1,5 @@
+package sec03.Q3;
+
+public abstract class HttpServlet {
+	public abstract void service();
+}
---
+++ chap07/src/sec03/Q3/HttpServletEx.java
@@ -0,0 +1,14 @@
+package sec03.Q3;
+
+public class HttpServletEx {
+
+	public static void main(String[] args) {
+		method(new LoginServlet());
+		method(new FileDownloadServlet());
+	}
+	
+	public static void method(HttpServlet servlet) {
+		servlet.service();
+	}
+
+}
