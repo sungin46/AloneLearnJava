1820dca5ddaaeb2769d5795cd9686aab96a00278
Envy <sungin0406@naver.com>
2023-12-26 18:16:07 +0900

    Feat #11 sec02 확인문제 풀이

---
+++ chap12/src/sec01/Q2/ThreadEx.java
@@ -0,0 +1,13 @@
+package sec01.Q2;
+
+public class ThreadEx {
+
+	public static void main(String[] args) {
+		Thread thread1 = new MovieThread();
+		thread1.start();
+		
+		Thread thread2 = new Thread(new MusicRunnable());
+		thread2.start();
+	}
+
+}
