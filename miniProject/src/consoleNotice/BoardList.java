776e07a8bf075a7aff1d19222d10446ddef3379c
Envy <sungin0406@naver.com>
2024-01-09 20:30:58 +0900

    Fix #15 추가 시 새로운 ArrayList에 추가해서 오류 발생. 파일을 일거온 리스트에 추가하도록 수정.

--- miniProject/src/consoleNotice/BoardList.java
+++ miniProject/src/consoleNotice/BoardList.java
@@ -36,11 +36,12 @@ public class BoardList {
 	}
 	
 	public void addList() throws Exception {
+		List<Board> addedList = readList();
 		System.out.println("타이틀, 내용, 작가를 순서대로 입력하세요.");
-		boardList.add(new Board(++counter, scanner.nextLine(), scanner.nextLine(), scanner.nextLine()));
+		addedList.add(new Board(++counter, scanner.nextLine(), scanner.nextLine(), scanner.nextLine()));
 		FileOutputStream fos = new FileOutputStream("/Users/sungin/Desktop/workspace/Eclipse/consoleBoard.db");
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(boardList);
+		oos.writeObject(addedList);
 		oos.flush();
 		oos.close();
 		list();
