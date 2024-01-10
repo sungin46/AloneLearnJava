8b1acbfe0fb97fc25d0cda62122607c3ad4af327
Envy <sungin0406@naver.com>
2024-01-10 20:25:51 +0900

    Feat #17 수정 메뉴 전체적인 틀

--- miniProject/src/consoleNotice/Modify.java
+++ miniProject/src/consoleNotice/Modify.java
@@ -1,7 +1,79 @@
 package consoleNotice;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Scanner;
+
 public class Modify {
-	public void modify() {
+	Menu menu = new Menu();
+	Scanner scanner = new Scanner(System.in);
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	
+	public void modify() throws Exception{
+		List<Board> addedList = AddedList.readList();
+		System.out.println("----------------------------------------------------------------");
+		for(Board board : addedList) {
+			System.out.println(board.getListNum() + "\t" + board.getTitle() + "\t" + board.getContent()
+			+ "\t" + board.getWriter() + "\t" + sdf.format(board.getDate()));
+		}
+		System.out.println("----------------------------------------------------------------");
+		System.out.println("1. 수정하기 | 2. 돌아가기");
+		System.out.println("----------------------------------------------------------------");
+		System.out.print("선택 : ");
+		String number = scanner.nextLine();
+		switch(number) {
+		case "1" : selectModify(); break;
+		case "2" : menu.showMenu(); break;
+		default : {
+			
+		}
+		}
+	}
+	
+	public void selectModify() {
+		try {
+			System.out.print("몇 번째 글을 수정하시겠습니까? : ");
+			int modifyNum = Integer.parseInt(scanner.nextLine());
+			System.out.println("----------------------------------------------------------------");
+			System.out.println("1. Title | 2. Content | 3. Writer | 4. ALL");
+			System.out.println("----------------------------------------------------------------");
+			System.out.print("어느 부분을 수정하시겠습니까? : ");
+			String number = scanner.nextLine();
+			switch (number) {
+			case "1": modifyTitle(modifyNum); break;
+			case "2": modifyContent(modifyNum); break;
+			case "3": modifyWriter(modifyNum);break;
+			case "4": modifyAll(modifyNum); break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + number);
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	
+	public void modifyAll(int modifyNum) throws Exception {
+		Board modifyList = AddedList.readList().get(modifyNum - 1);
+		System.out.println("전체 수정 - " + modifyNum);
+	}
+	
+	public void modifyTitle(int modifyNum) throws Exception {
+		Board modifyList = AddedList.readList().get(modifyNum - 1);
+		System.out.println("타이틀 수정 - " + modifyNum);
+	}
+	
+	public void modifyContent(int modifyNum) throws Exception {
+		Board modifyList = AddedList.readList().get(modifyNum - 1);
+		System.out.println("컨텐츠 수정 - " + modifyNum);
+	}
+	
+	public void modifyWriter(int modifyNum) throws Exception {
+		Board modifyList = AddedList.readList().get(modifyNum - 1);
+		System.out.println("작가 수정 - " + modifyNum);
 	}
 }
