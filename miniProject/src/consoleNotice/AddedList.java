3246e74d574f717e74e298ae01893f536c0d7611
Envy <sungin0406@naver.com>
2024-01-16 17:33:00 +0900

    Feat #14 db파일 수정하는 부분을 함수로 쪼개서 부르도록 수정

--- miniProject/src/consoleNotice/AddedList.java
+++ miniProject/src/consoleNotice/AddedList.java
@@ -1,7 +1,9 @@
 package consoleNotice;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class AddedList {
@@ -10,5 +12,14 @@ public class AddedList {
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		List<Board> list = (List<Board>) ois.readObject();
 		return list;
+	}
+	
+	public static void writeFile(List<Board> boards) throws Exception {
+		FileOutputStream fos = new FileOutputStream("/Users/sungin/Desktop/workspace/Eclipse/consoleBoard.db");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+		oos.writeObject(boards);
+		oos.flush();
+		oos.close();
 	}
 }
