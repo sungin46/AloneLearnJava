5251bc703414293d259ab161a6b3477ef6cc9207
Envy <sungin0406@naver.com>
2024-01-17 18:06:22 +0900

    Feat #19 txt파일 저장 기능 구현

--- miniProject/src/consoleNotice/SaveFile.java
+++ miniProject/src/consoleNotice/SaveFile.java
@@ -1,7 +1,41 @@
 package consoleNotice;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.NotSerializableException;
+
 public class SaveFile {
-	public void saveFile() {
+	Menu menu = new Menu();
+	String txtPath = "/Users/sungin/Desktop/workspace/Eclipse/consoleBoard.txt";
+	File txtFile = new File(txtPath);
+	
+	public void saveFile() throws Exception{
+		System.out.println(".db 파일을 txt 파일로 저장합니다.");
+		saveTxtFile();
+		menu.showMenu();
+	}
+	
+
+	public void saveTxtFile() throws Exception {
+		
+		try {
+			boolean isExists = txtFile.exists();
+			if(!isExists) txtFile.createNewFile();
+			
+			FileWriter fileWriter = new FileWriter(txtFile);
+			String list = null;
+			for(Board board : AddedList.readList()) {
+				// 객체로 저장하면 텍스트 파일이 깨지기 때문에 String 형태로 변환시켜 저장한다.
+				list = board.getListNum()+","+board.getTitle()+","+board.getContent()+","+board.getWriter()+","+board.getDate()+"\n";
+				fileWriter.write(list);
+			}
+			
+			fileWriter.close();
+			
+		} catch (NotSerializableException e) {
+			System.out.println("NotSerializableException 제외 발생." + e);
+			menu.showMenu();
+		}
 		
 	}
 }
